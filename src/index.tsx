ef4112ac21154b4414614d51d8c8b77e69d67201
craigkehl <keh19003@byui.edu>
2022-02-05 19:15:00 -0700

    router start

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App className='app' />,
+  <BrowserRouter>
+    <App className='app' />
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,5 @@
+const Welcome = () => {
+  return <h1>Camera Controller</h1>;
+};
+
+export default Welcome;
