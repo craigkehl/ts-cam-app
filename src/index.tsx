b900e7c353a3c8adb87ee26d4f751988a45b0187
Craig Kehl <keh19003@byui.edu>
2022-04-20 22:42:00 -0600

    update React18

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
-import './index.css';
-import App from './App';
-import configurePresetStore from './store/presets-store';
-import configurePtzStore from './store/ptz-store';
-import configureSceneStore from './store/scenes-store';
+import "./index.css"
+import App from "./App"
+import configurePresetStore from "./store/presets-store"
+import configurePtzStore from "./store/ptz-store"
+import configureSceneStore from "./store/scenes-store"
 
-configurePresetStore();
-configurePtzStore();
-configureSceneStore();
+configurePresetStore()
+configurePtzStore()
+configureSceneStore()
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App className='app' />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+const root = ReactDOM.createRoot(document.getElementById("root")!)
+root.render(
   <BrowserRouter>
-    <App className='app' />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+    <App />
+  </BrowserRouter>
+)
