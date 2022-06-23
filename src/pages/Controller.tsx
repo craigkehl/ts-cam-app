8f8ef6ede50446fd445ab9f6a5b66b7116a12579
craigkehl <keh19003@byui.edu>
2022-06-22 19:21:59 -0600

    fix yMax parameter value

--- src/pages/Controller.tsx
+++ src/pages/Controller.tsx
@@ -8,28 +8,28 @@ import { useStore } from '../store/store';
 import classes from './Controller.module.css';
 
 const Controller: React.FC<{ className?: string }> = props => {
-  const globalState = useStore()[0];
+    const globalState = useStore()[0];
 
-  return (
-    <div className={`${classes['controller-page']} ${props.className}`}>
-      <SceneBtns
-        className={`${classes['scene-btns']} ${props.className}`}
-        action='recallScene'
-      />
-      <Card className={`${classes['preset-btns']} ${props.className}`}>
-        <PresetBtnGrp action='recallPreset' />
-      </Card>
-      <Card className={`${classes['pan-tilt-btns']} ${props.className}`}>
-        <Slider className={`${classes} ${props.className}`} />
-        <DoubleSlider
-          className={`${classes} ${props.className}`}
-          xMax='24'
-          yMax='24'
-          resolution={globalState.ptzSettings.resolution}
-        />
-      </Card>
-    </div>
-  );
+    return (
+        <div className={`${classes['controller-page']} ${props.className}`}>
+            <SceneBtns
+                className={`${classes['scene-btns']} ${props.className}`}
+                action='recallScene'
+            />
+            <Card className={`${classes['preset-btns']} ${props.className}`}>
+                <PresetBtnGrp action='recallPreset' />
+            </Card>
+            <Card className={`${classes['pan-tilt-btns']} ${props.className}`}>
+                <Slider className={`${classes} ${props.className}`} />
+                <DoubleSlider
+                    className={`${classes} ${props.className}`}
+                    xMax='24'
+                    yMax='20'
+                    resolution={globalState.ptzSettings.resolution}
+                />
+            </Card>
+        </div>
+    );
 };
 
 export default Controller;
