4818ed96090c694af46ac83c6269674f191554ca
craigkehl <keh19003@byui.edu>
2022-10-29 17:40:47 -0600

    material layout

--- src/components/Presets/Preset.tsx
+++ src/components/Presets/Preset.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import Button from '../UI/Button';
+// import Button from '../UI/Button';
+import Button from '@mui/material/Button';
 
 import classes from './Preset.module.css';
 
 interface PresetProps extends React.PropsWithChildren<object> {
-  className?: string;
-  id: number;
-  name: string;
-  isShow: boolean;
-  isCurrent: boolean;
-  onRecallPreset: (id: number) => void;
+    className?: string;
+    id: number;
+    name: string;
+    isShow: boolean;
+    isCurrent: boolean;
+    onRecallPreset: (id: number) => void;
 }
 
 const Preset = React.memo((props: PresetProps) => {
-  const recallPresetHandler = (id: number) => {
-    props.onRecallPreset(id);
-  };
+    const recallPresetHandler = (id: number) => {
+        props.onRecallPreset(id);
+    };
 
-  return (
-    <Button
-      className={`${classes.btn} ${props.className} ${
-        props.isCurrent && classes.selected
-      }`}
-      onClick={() => recallPresetHandler(props.id)}
-    >
-      {props.name}
-    </Button>
-  );
+    return (
+        <Button
+            variant='contained'
+            className={`${classes.btn} ${props.className} ${
+                props.isCurrent && classes.selected
+            }`}
+            onClick={() => recallPresetHandler(props.id)}
+        >
+            {props.name}
+        </Button>
+    );
 });
 
 export default Preset;
--- src/components/Presets/PresetBtnGrp.tsx
+++ src/components/Presets/PresetBtnGrp.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
 
 import { useStore } from '../../store/store';
 import { PresetState } from '../../store/presets-store';
@@ -7,62 +8,71 @@ import { recallPreset } from '../../util/http-requests';
 import classes from './PresetBtnGrp.module.css';
 
 const PresetBtnGrp: React.FC<{
-  className?: string;
-  action?: string;
-  list?: string;
-}> = (props) => {
-  const [state, dispatch] = useStore();
-  const presets: PresetState[] = state.presets;
+    className?: string;
+    action?: string;
+    list?: string;
+}> = props => {
+    const [state, dispatch] = useStore();
+    const presets: PresetState[] = state.presets;
 
-  let showHiddenList = props.list === 'showHidden' ? true : false;
+    let showHiddenList = props.list === 'showHidden' ? true : false;
 
-  const recallPresetHandler = (id: number) => {
-    switch (props.action) {
-      case 'recallPreset':
-        recallPreset(id.toString());
-        dispatch('CURRENT_PRESET', id);
-        break;
-      case 'toggleShow':
-        dispatch('TOGGLE_SHOW_PRESET', id);
-        break;
-      default:
-        return;
-    }
-  };
+    const recallPresetHandler = (id: number) => {
+        switch (props.action) {
+            case 'recallPreset':
+                recallPreset(id.toString());
+                dispatch('CURRENT_PRESET', id);
+                break;
+            case 'toggleShow':
+                dispatch('TOGGLE_SHOW_PRESET', id);
+                break;
+            default:
+                return;
+        }
+    };
 
-  const presetList = (
-    <>
-      <h3 className={`${classes.title} ${props.className}`}>
-        {showHiddenList ? 'Hidden ' : ''} Presets
-      </h3>
-      <div className={`${classes.btnGrp} ${props.className}`}>
-        {presets.length > 0 ? (
-          presets.map(
-            (preset: PresetState) =>
-              (showHiddenList ? !preset.isShow : preset.isShow) && (
-                <Preset
-                  className={`${classes.btn}  ${
-                    classes[showHiddenList ? 'hidden' : '']
-                  }`}
-                  key={preset.id}
-                  id={preset.id}
-                  name={preset.name}
-                  isShow={preset.isShow}
-                  isCurrent={preset.isCurrent}
-                  onRecallPreset={() => recallPresetHandler(preset.id)}
-                >
-                  {preset.name}
-                </Preset>
-              )
-          )
-        ) : (
-          <p>No presets found.</p>
-        )}
-      </div>
-    </>
-  );
+    const presetList = (
+        <>
+            <h3 className={`${classes.title} ${props.className}`}>
+                {showHiddenList ? 'Hidden ' : ''} Presets
+            </h3>
+            <Stack
+                spacing={1}
+                direction='row'
+                flexWrap='wrap'
+                className={`${classes.btnGrp} ${props.className}`}
+            >
+                {presets.length > 0 ? (
+                    presets.map(
+                        (preset: PresetState) =>
+                            (showHiddenList
+                                ? !preset.isShow
+                                : preset.isShow) && (
+                                <Preset
+                                    className={`${classes.btn}  ${
+                                        classes[showHiddenList ? 'hidden' : '']
+                                    }`}
+                                    key={preset.id}
+                                    id={preset.id}
+                                    name={preset.name}
+                                    isShow={preset.isShow}
+                                    isCurrent={preset.isCurrent}
+                                    onRecallPreset={() =>
+                                        recallPresetHandler(preset.id)
+                                    }
+                                >
+                                    {preset.name}
+                                </Preset>
+                            )
+                    )
+                ) : (
+                    <p>No presets found.</p>
+                )}
+            </Stack>
+        </>
+    );
 
-  return <>{presetList}</>;
+    return <>{presetList}</>;
 };
 
 export default PresetBtnGrp;
